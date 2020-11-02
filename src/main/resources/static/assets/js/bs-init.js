f83f3a90571ac22fbfe1eacc71bb1ad839cceff0
chan <lsc4237@gmail.com>
2020-11-03 01:30:33 +0900

    갯수 카운터 할 시 금액 변경 수정

--- src/main/resources/static/assets/js/bs-init.js
+++ src/main/resources/static/assets/js/bs-init.js
@@ -7,11 +7,25 @@ $(document).ready(function(){
 		    $('.count123').prop('disabled', true);
    			$(document).on('click','.plus',function(){
 				$(this).prev().val(parseInt($(this).prev().val()) + 1 );
+				var length = $(".bookSeq").length;
+                money = 0;
+                for(var i=0; i<length; i++){
+                  money = money + (parseInt($(".count123").eq(i).val())*parseInt($(".bookPrice").eq(i).val()))
+                }
+                $("#money").text("상품 가격: "+ money)
     		});
         	$(document).on('click','.minus',function(){
     			$(this).next().val(parseInt($(this).next().val()) - 1 );
     				if ($(this).next().val() == 0) {
 						$(this).next().val(1);
 					}
+					var length = $(".bookSeq").length;
+                    money = 0;
+                    for(var i=0; i<length; i++){
+                      money = money + (parseInt($(".count123").eq(i).val())*parseInt($(".bookPrice").eq(i).val()))
+                    }
+
+                $("#money").text("상품 가격: "+ money)
     	    	});
+
  		});
